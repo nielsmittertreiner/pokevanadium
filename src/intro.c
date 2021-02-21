f0b1c7701b6185a78799f56a2a81b5044258242d
Niels Mittertreiner <mittertreinerniels@gmail.com>
2021-02-21 16:35:49 +0100

    Skip intro cinematic

--- src/intro.c
+++ src/intro.c
@@ -1034,7 +1034,7 @@ static u8 SetUpCopyrightScreen(void)
         if (UpdatePaletteFade())
             break;
         CreateTask(Task_IntroLoadPart1Graphics, 0);
-        SetMainCallback2(MainCB2_Intro);
+        SetMainCallback2(CB2_InitTitleScreen);
         if (gMultibootProgramStruct.gcmb_field_2 != 0)
         {
             if (gMultibootProgramStruct.gcmb_field_2 == 2)
